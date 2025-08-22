272bebe1fbdec99060923fd14c7d7d8dd0892b9a
venkatesh peta <85110131+DigiStackOps@users.noreply.github.com>
2025-08-22 20:43:17 +0530

    Update initdb.js

--- backend/initdb.js
+++ backend/initdb.js
@@ -8,3 +8,35 @@ db.createUser({
 
 db.createCollection("users")
 
+db.users.insertMany([
+  {
+    name: "venkatesh",
+    email: "venkatesh@sapsecops.com",
+    designation: "DevSecOps Engineer",
+    salary: 1500000
+  },
+  {
+    name: "chaitanya",
+    email: "chaitanya@sapsecops.com",
+    designation: "QA Engineer",
+    salary: 1200000
+  },
+  {
+    name: "padol",
+    email: "padol@sapsecops.com",
+    designation: "SAP Developer",
+    salary: 1100000
+  },
+  {
+    name: "pandu",
+    email: "pandu@sapsecops.com",
+    designation: "Database Admin",
+    salary: 1000000
+  },
+  {
+    name: "Ganesh",
+    email: "ganesh@sapsecops.com",
+    designation: "Manager",
+    salary: 1300000
+  }
+])
