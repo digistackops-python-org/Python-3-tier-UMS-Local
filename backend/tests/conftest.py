0fc8ffcfe76199ad5a8a259abc8cc69a11a72b7f
venkatesh peta <85110131+DigiStackOps@users.noreply.github.com>
2026-02-08 08:45:29 +0530

    Create conftest.py

---
+++ backend/tests/conftest.py
@@ -0,0 +1,9 @@
+# backend/tests/conftest.py
+import pytest
+from backend.app import app
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
